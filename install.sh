29cbb09b9d178bf0833a0e007efebd4f9ab1e4a5
aniketstark <santoshmhatre330@gmail.com>
2019-09-30 07:26:45 +0530

    fixed install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ green='\033[1;32m'
 printf "$green"
 echo "The Modules file size 50.mb
 After Extraction it will only 100.mb" | pv -qL 10
-pkg install -y git curl php python python2 wget termux-api zip
+pkg install -y pv  git curl php python python2 wget termux-api zip
 pip2 install beautifulsoup4
 pip2 install termcolor
 pip2 install argparse
